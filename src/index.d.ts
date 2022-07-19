79be41e7c9b45d06b00956512c3e8505c78a819e
Matte <51192395+MatteZ02@users.noreply.github.com>
2022-07-19 22:51:16 +0300

    types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,52 @@
 declare module "musix-api-types" {
-    export interface PlayerStatusData {
-        id: string;
-        data: {
-            playing: boolean;
-            paused: boolean;
-            looping: boolean;
-            songLooping: boolean;
-        };
-    }
+    interface Song {
+    title: string;
+    author: string;
+    url: string;
+    duration: string;
+}
+
+interface PlayerStatus {
+    playing: boolean;
+    paused: boolean;
+    looping: boolean;
+    songLooping: boolean;
+    stopped: boolean;
+    errored: boolean;
+    startTime: null | number;
+}
+
+export interface playerData {
+    id: string;
+    queue: Song[];
+    status: PlayerStatus;
+    event: "start" | "end" | "update" | "created" | "deleted";
+}
+
+export interface statsData {
+    commandUses: { command: string; uses: number }[];
+}
+
+export interface debugData {}
+
+export interface Payload {
+    type: "player" | "stats" | "debug";
+    data: playerData | statsData | debugData;
+}
+
+export interface PlayerPayload extends Payload {
+    type: "player";
+    data: playerData;
+}
+
+export interface StatsPayload extends Payload {
+    type: "stats";
+    data: statsData;
+}
+
+export interface DebugPayload extends Payload {
+    type: "debug";
+    data: debugData;
+}
+
 }
