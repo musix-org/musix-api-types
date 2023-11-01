cc50e17782388c4e289adac51cbb6d4ca45993b5
Matte <51192395+MatteZ02@users.noreply.github.com>
2023-11-01 17:06:09 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-enum PayloadType {
-    statusUpdate = "statusUpdate",
-    playerStatusUpdate = "playerStatusUpdate",
-    playerUpdate = "playerUpdate",
-    queueUpdate = "queueUpdate",
-    systemStatusUpdate = "systemStatusUpdate",
-    shardStatusUpdate = "shardStatusUpdate",
-    auth = "auth"
-}
+const PayloadType = {
+    statusUpdate: "statusUpdate",
+    playerStatusUpdate: "playerStatusUpdate",
+    playerUpdate: "playerUpdate",
+    queueUpdate: "queueUpdate",
+    systemStatusUpdate: "systemStatusUpdate",
+    shardStatusUpdate: "shardStatusUpdate",
+    auth: "auth"
+};
 
 export { PayloadType };
