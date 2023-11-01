eb8f22f6e9371e7844765ef3c620c9c3797caddb
Matte <51192395+MatteZ02@users.noreply.github.com>
2023-11-01 17:03:11 +0200

    export enum

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export enum PayloadType {
+    statusUpdate = "statusUpdate",
+    playerStatusUpdate = "playerStatusUpdate",
+    playerUpdate = "playerUpdate",
+    queueUpdate = "queueUpdate",
+    systemStatusUpdate = "systemStatusUpdate",
+    shardStatusUpdate = "shardStatusUpdate",
+    auth = "auth"
+}
