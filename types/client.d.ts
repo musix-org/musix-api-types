fdd861cacd4833d18f37f236d2c7f8592d65e0fa
Matte <51192395+MatteZ02@users.noreply.github.com>
2024-03-01 09:37:15 +0200

    Update client.d.ts

--- types/client.d.ts
+++ types/client.d.ts
@@ -1,7 +1,10 @@
+import { RESTAPIPartialCurrentUserGuild } from "discord-api-types/v10";
+
 export interface ClientData {
     fullyReady: boolean;
     ready: boolean;
     readyTimestamp: number | null;
     shard: number[] | null;
     uptime: number | null;
+    guilds?: RESTAPIPartialCurrentUserGuild[];
 }
