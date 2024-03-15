73ab0cf32067ab29a300fcd8d963695db17afebf
Matte <51192395+MatteZ02@users.noreply.github.com>
2024-03-15 18:13:54 +0200

    Update playlist.d.ts

--- types/playlist.d.ts
+++ types/playlist.d.ts
@@ -3,7 +3,7 @@ import { Track } from "./queue";
 export type PlaylistTrack = Omit<Track, "position">;
 
 export interface Playlist {
-    id: string;
+    id: number;
     user_id: string;
     name: string;
     tracks: PlaylistTrack[];
